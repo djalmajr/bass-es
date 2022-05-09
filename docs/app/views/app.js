59dcfff8e0ac2b6c3258caf2b7d123fdd17335a3
djalmajr <djalmajr@gmail.com>
2022-05-09 10:25:26 -0300

    Ajustes..

--- docs/app/views/app.js
+++ docs/app/views/app.js
@@ -1,9 +1,7 @@
-import { Base, html } from 'min-ce/lib/base.js';
+import { Base, define, html } from 'min-ce/lib/base.js';
 import styles from './app.css' assert { type: 'css' };
 
 export class App extends Base {
-  static tagName = 'v-app';
-
   static styles = styles;
 
   props = {
@@ -11,50 +9,45 @@ export class App extends Base {
     opened: false,
   };
 
-  // connectedCallback() {
-  //   super.connectedCallback();
-  //   this.#updateCounter();
-  // }
+  connectedCallback() {
+    super.connectedCallback();
+    this.#updateCounter();
+  }
 
-  // #updateCounter = () => {
-  //   this.counter++;
-  //   setTimeout(this.#updateCounter, 1000);
-  // };
+  #updateCounter = () => {
+    this.counter++;
+    setTimeout(this.#updateCounter, 1000);
+  };
 
   #toggle = () => {
     this.opened = !this.opened;
   };
 
   render() {
-    return html` <m-button intent="primary" loading>Loading</m-button> `;
+    return html`
+      <h1>Counter: ${this.counter}</h1>
+      <m-space align="center">
+        <m-button intent="primary" @click=${this.#toggle}>Toggle Modal</m-button>
+        <m-button intent="primary" disabled>Disabled</m-button>
+        <m-button intent="primary" loading>Loading</m-button>
+      </m-space>
+      <m-dialog mask-closable ?opened=${this.opened} @close=${this.#toggle}>
+        <strong slot="title">Save Info</strong>
+        <m-space direction="vertical">
+          <form>
+            <m-space direction="vertical" size="tiny">
+              <span>Name</span>
+              <input name="name" />
+            </m-space>
+          </form>
+          <footer>
+            <m-button intent="primary" @click=${this.#toggle}>Salvar</m-button>
+            <m-button variant="outline" @click=${this.#toggle}>Close</m-button>
+          </footer>
+        </m-space>
+      </m-dialog>
+    `;
   }
-  // render() {
-  //   return html`
-  //     <h1>Counter: ${this.counter}</h1>
-  //     <m-space align="center">
-  //       <m-button intent="primary" @click=${this.#toggle}>Toggle Modal</m-button>
-  //       <m-button intent="primary" disabled>Disabled</m-button>
-  //       <m-button intent="primary" loading>Loading</m-button>
-  //     </m-space>
-  //     <m-dialog mask-closable ?opened=${this.opened} @close=${this.#toggle}>
-  //       <strong slot="title">Save Info</strong>
-  //       <m-space direction="vertical">
-  //         <form>
-  //           <m-space direction="vertical" size="tiny">
-  //             <span>Name</span>
-  //             <input name="name" />
-  //           </m-space>
-  //         </form>
-  //         <footer>
-  //           <m-button intent="primary" @click=${this.#toggle}>Salvar</m-button>
-  //           <m-button variant="outline" @click=${this.#toggle}>Close</m-button>
-  //         </footer>
-  //       </m-space>
-  //     </m-dialog>
-  //   `;
-  // }
 }
 
-if (!customElements.get(App.tagName)) {
-  customElements.define(App.tagName, App);
-}
+define('v-app', App);
